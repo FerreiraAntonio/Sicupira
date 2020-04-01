6a87c90aeb8c014453810243df24544093a5c58a
Antonio Horta <antonio@horta.net.br>
2020-04-01 15:34:58 -0300

    ajustes modelo e busca de discente e docente

--- pessoa/models.py
+++ pessoa/models.py
@@ -71,6 +71,7 @@ class Discente(models.Model):
                                     on_delete=models.SET_NULL,
                                     null=True, blank=True,
                                     related_name='SituacaoMatricula')
+    ano = models.IntegerField(blank=True, null=True)
     data_situacao = models.DateField()
     nivel = models.ForeignKey(SicupiraModel.NivelGraduacao,
                                  on_delete=models.SET_NULL,
@@ -102,7 +103,16 @@ class Docente(models.Model):
     titulo_pais = models.ForeignKey(SicupiraModel.Pais,
                                        on_delete=models.SET_NULL,
                                        null=True, blank=True,
-                                       related_name='TituloPais')
+                                       related_name='TituloPais')    
+    programa = models.ForeignKey(SicupiraModel.Programa,
+                                           on_delete=models.SET_NULL,
+                                           null=True, blank=True,
+                                           related_name='ProgramaDocente')
+    ano = models.IntegerField(blank=True, null=True)
+    categoria = models.ForeignKey(SicupiraModel.CategoriaDocente,
+                                     on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='CategoriaDocente')
     regime_trabalho = models.ForeignKey(SicupiraModel.RegimeTrabalho,
                                            on_delete=models.SET_NULL,
                                            null=True, blank=True,
@@ -122,24 +132,24 @@ class Docente(models.Model):
 ##################################################
 # Inicio do Bloco [Vinculo com Instituição]
 ##################################################
-class Vinculo(models.Model):
-    pessoa = models.ForeignKey(Docente,
-                                  on_delete=models.CASCADE,
-                                  related_name='Docente')
-    instituicao = models.ForeignKey(SicupiraModel.Instituicao,
-                                       on_delete=models.CASCADE,
-                                       related_name='Instituicao')
-    data_inicio = models.DateField()
-    categoria = models.ForeignKey(SicupiraModel.CategoriaDocente,
-                                     on_delete=models.SET_NULL,
-                                     null=True, blank=True,
-                                     related_name='CategoriaDocente')
-
-    def __str__(self):
-        return '%s - %s' % (self.Docente.nome, self.Instituicao.nome)
-
-    def get_absolute_url(self):
-        return reverse('vinculo_edit', kwargs={'pk': self.pk})
+# class Vinculo(models.Model):
+#     pessoa = models.ForeignKey(Docente,
+#                                   on_delete=models.CASCADE,
+#                                   related_name='Docente')
+#     instituicao = models.ForeignKey(SicupiraModel.Instituicao,
+#                                        on_delete=models.CASCADE,
+#                                        related_name='Instituicao')
+#     data_inicio = models.DateField()
+#     categoria = models.ForeignKey(SicupiraModel.CategoriaDocente,
+#                                      on_delete=models.SET_NULL,
+#                                      null=True, blank=True,
+#                                      related_name='CategoriaDocente')
+# 
+#     def __str__(self):
+#         return '%s - %s' % (self.Docente.nome, self.Instituicao.nome)
+# 
+#     def get_absolute_url(self):
+#         return reverse('vinculo_edit', kwargs={'pk': self.pk})
 
 
 ##################################################
--- pessoa/views.py
+++ pessoa/views.py
@@ -8,13 +8,13 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-#from .models import *
+# from .models import *
 from .forms import *
 from pessoa.models import Pessoa
 from pessoa.models import Discente
 from pessoa.models import Docente
-from pessoa.models import Vinculo
-from pessoa.models import Orienta
+# from pessoa.models import Vinculo
+# from pessoa.models import Orienta
 from pessoa.models import Abreviatura
 
 from django.contrib.auth.decorators import login_required
@@ -57,24 +57,14 @@ class DiscenteList(ListView):
 
         if 'nome' in self.request.GET:
             queryset = queryset.filter(pessoa__nome__icontains=self.request.GET['nome'])
-        if 'sexo' in self.request.GET:
-            queryset = queryset.filter(pessoa__sexo__desc_sexo__icontains=self.request.GET['sexo'])
-        if 'data_nascimento' in self.request.GET:
-            queryset = queryset.filter(pessoa__data_nascimento__icontains=self.request.GET['data_nascimento'])
-        if 'numero_documento' in self.request.GET:
-            queryset = queryset.filter(pessoa__numero_documento__icontains=self.request.GET['numero_documento'])
-        if 'tipo_documento' in self.request.GET:
-            queryset = queryset.filter(pessoa__tipo_documento__desc_tipo_doc__icontains=self.request.GET['tipo_documento'])
-        if 'email' in self.request.GET:
-            queryset = queryset.filter(pessoa__email__icontains=self.request.GET['email'])
-        if 'nacionalidade' in self.request.GET:
-            queryset = queryset.filter(pessoa__nacionalidade__nome_pais__icontains=self.request.GET['nacionalidade'])
-        if 'curso' in self.request.GET:
-            queryset = queryset.filter(curso__nome_curso__icontains=self.request.GET['curso'])
+        if 'instituicao' in self.request.GET:
+            queryset = queryset.filter(curso__programa_id__instituicao__nome__icontains=self.request.GET['instituicao'])
+        if 'programa' in self.request.GET:
+            queryset = queryset.filter(curso__programa_id__nome_programa__icontains=self.request.GET['programa'])
+        if 'ano' in self.request.GET:
+            queryset = queryset.filter(ano__icontains=self.request.GET['ano'])
         if 'situacao' in self.request.GET:
             queryset = queryset.filter(situacao__desc_situacao_matricula__icontains=self.request.GET['situacao'])
-        if 'data_situacao' in self.request.GET:
-            queryset = queryset.filter(data_situacao__icontains=self.request.GET['data_situacao'])
         if 'nivel' in self.request.GET:
             queryset = queryset.filter(nivel__desc_nivel_graduacao__icontains=self.request.GET['nivel'])
 
@@ -172,30 +162,16 @@ class DocenteList(ListView):
 
         if 'nome' in self.request.GET:
             queryset = queryset.filter(pessoa__nome__icontains=self.request.GET['nome'])
-        if 'sexo' in self.request.GET:
-            queryset = queryset.filter(pessoa__sexo__desc_sexo__icontains=self.request.GET['sexo'])
-        if 'data_nascimento' in self.request.GET:
-            queryset = queryset.filter(pessoa__data_nascimento__icontains=self.request.GET['data_nascimento'])
-        if 'numero_documento' in self.request.GET:
-            queryset = queryset.filter(pessoa__numero_documento__icontains=self.request.GET['numero_documento'])
-        if 'tipo_documento' in self.request.GET:
-            queryset = queryset.filter(pessoa__tipo_documento__desc_tipo_doc__icontains=self.request.GET['tipo_documento'])
-        if 'email' in self.request.GET:
-            queryset = queryset.filter(pessoa__email__icontains=self.request.GET['email'])
-        if 'nacionalidade' in self.request.GET:
-            queryset = queryset.filter(pessoa__nacionalidade__nome_pais__icontains=self.request.GET['nacionalidade'])
-        if 'titulo_nivel' in self.request.GET:
-            queryset = queryset.filter(titulo_nivel__desc_nivel_graduacao__icontains=self.request.GET['titulo_nivel'])
-        if 'data_titulacao' in self.request.GET:
-            queryset = queryset.filter(data_titulacao__icontains=self.request.GET['data_titulacao'])
-        if 'area_conhecimento' in self.request.GET:
-            queryset = queryset.filter(area_conhecimento__icontains=self.request.GET['area_conhecimento'])
-        if 'titulo_pais' in self.request.GET:
-            queryset = queryset.filter(titulo_pais__nome_pais__icontains=self.request.GET['titulo_pais'])
+        if 'instituicao' in self.request.GET:
+            queryset = queryset.filter(programa__instituicao__nome__icontains=self.request.GET['instituicao'])
+        if 'programa' in self.request.GET:
+            queryset = queryset.filter(programa__nome_programa__icontains=self.request.GET['programa'])
+        if 'ano' in self.request.GET:
+            queryset = queryset.filter(ano__icontains=self.request.GET['ano'])
         if 'regime_trabalho' in self.request.GET:
             queryset = queryset.filter(regime_trabalho__desc_regime_trabalho__icontains=self.request.GET['regime_trabalho'])
-        if 'vinculo_ies' in self.request.GET:
-            queryset = queryset.filter(vinculo_ies__desc_vinculo_ies__icontains=self.request.GET['vinculo_ies'])
+        if 'categoria' in self.request.GET:
+            queryset = queryset.filter(categoria__desc_categoria__icontains=self.request.GET['categoria'])
 
         return queryset
 
@@ -214,14 +190,14 @@ def docente_detail_view(request, id):
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class DocenteCreate(CreateView):
     model = Docente
-    fields = ['pessoa', 'titulo_nivel', 'data_titulacao', 'titulo_pais', 'regime_trabalho', 'vinculo_ies']
+    fields = ['pessoa', 'titulo_nivel', 'data_titulacao', 'titulo_pais', 'regime_trabalho', 'vinculo_ies', 'programa', 'ano', 'categoria']
     success_url = reverse_lazy('docente_list')
 
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class DocenteUpdate(UpdateView):
     model = Docente
-    fields = ['pessoa', 'titulo_nivel', 'data_titulacao', 'titulo_pais', 'regime_trabalho', 'vinculo_ies']
+    fields = ['pessoa', 'titulo_nivel', 'data_titulacao', 'titulo_pais', 'regime_trabalho', 'vinculo_ies', 'programa', 'ano', 'categoria']
     success_url = reverse_lazy('docente_list')
 
 
